9e8d5344349ea3522e00c17d8e598dfeb8a98b34
DewanggaDew <dewangga.indera@gmail.com>
2025-07-17 12:26:33 +0800

    Update development environment setup to use tailwindv4 + vite + react typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<div className="flex items-center justify-center h-screen">
+				<h1 className="text-3xl font-bold underline hover:text-red-500">
+					Hello World!
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
